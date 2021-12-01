0679a63b4c67f4adc42a0c2ac67090ff4f8cb015
ektmf7890 <yundayoung1028@gmail.com>
2021-12-02 00:59:22 +0900

    ?

--- backend/pill_ai/views.py
+++ backend/pill_ai/views.py
@@ -2,17 +2,29 @@ from rest_framework.decorators import api_view
 from django.conf import settings
 from rest_framework.response import Response
 
+from pharmasee.models import Pill
+
 import os
 import pandas
+import tensorflow as tf
+import tensorflow_hub as hub
+import warnings
+warnings.filterwarnings('ignore')
 
 # bulk_create()
 def populate_pills(csv_url):
     df = pandas.read_csv(csv_url)
-    print(df.head())
     
 
 @api_view(['POST'])
 def pill_ai_identify(request):
-    csv_url = os.path.join(settings.STATIC_ROOT, 'csv/sample_btm.csv')
-    populate_pills(csv_url)
+    if Pill.objects.count() < 10:
+        csv_url = os.path.join(settings.STATIC_ROOT, 'csv/sample_btm.csv')
+        populate_pills(csv_url)
+
+    model_url  = os.path.join(settings.STATIC_ROOT, 'h5/best_model.h5')
+    with tf.device('/cpu:0'):
+        model = tf.keras.models.load_model(model_url, custom_objects={'KerasLayer': hub.KerasLayer})
+    # print(model.summary())
+
     return Response({})
