72fee0fbdc53b4bd5d8ad1ff31406e2f559a3427
ektmf7890 <yundayoung1028@gmail.com>
2021-12-06 18:12:52 +0900

    nugu proxy 재작성

--- backend/nugu/urls.py
+++ backend/nugu/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import answer_taken_pills, answer_not_taken_pills, answer_total_status, health
 
 urlpatterns = [
-    path('answer.taken_pills', answer_taken_pills, name='ans-taken-pills'),
-    path('answer.not_taken_pills', answer_not_taken_pills, name='ans-not-taken-pills'),
+    path('answer.taken_pills', answer_taken_pills, name='answer-taken-pills'),
+    path('answer.not_taken_pills', answer_not_taken_pills, name='answer-not-taken-pills'),
     path('answer.total_stauts', answer_total_status, name='answer-total-status'),
     path('health', health, name='health')
 ]
